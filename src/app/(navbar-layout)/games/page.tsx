56c9fd95cbfd53d71af3faf7a8e321daf1e93af2
Tonkata-hub <tonyyankOV07@gmail.com>
2026-02-03 20:05:46 +0200

    made /games a coming soon page

--- src/app/(navbar-layout)/games/page.tsx
+++ src/app/(navbar-layout)/games/page.tsx
@@ -1,20 +1,20 @@
-import GamesView from '@/domains/games/GamesView';
+// import GamesView from '@/domains/games/GamesView';
 
-const GamesPage = () => {
-  return <GamesView />;
-};
-
-export default GamesPage;
-
-// import { ComingSoon } from '@/components/ComingSoon';
-
-// const page = () => {
-//   return (
-//     <ComingSoon
-//       title="Games"
-//       description="Game schedules, live scores, and highlights will be available soon. Don't miss the action!!!"
-//     />
-//   );
+// const GamesPage = () => {
+//   return <GamesView />;
 // };
 
-// export default page;
+// export default GamesPage;
+
+import { ComingSoon } from '@/components/ComingSoon';
+
+const page = () => {
+  return (
+    <ComingSoon
+      title="Games"
+      description="Game schedules, live scores, and highlights will be available soon. Don't miss the action!!!"
+    />
+  );
+};
+
+export default page;
