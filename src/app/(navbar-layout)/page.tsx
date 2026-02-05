980c42cce10225a3562da7ef0e49d6316ab060db
Tonkata-hub <tonyyankOV07@gmail.com>
2026-02-05 23:39:03 +0200

    removed unnecesary force-dynamic exports

--- src/app/(navbar-layout)/page.tsx
+++ src/app/(navbar-layout)/page.tsx
@@ -1,12 +1,11 @@
 import HomeView from '@/domains/home/HomeView';
 import { getHomePageData } from '@/domains/home/ssr/getHomePageData';
 
-// Avoid prerender at build time (API may be unavailable); render on request
-export const dynamic = 'force-dynamic';
-
 const page = async () => {
   const { teams, news, matches, heroStatistics, topPlayers } = await getHomePageData();
-  return <HomeView teams={teams} news={news} matches={matches} heroStatistics={heroStatistics} topPlayers={topPlayers} />;
+  return (
+    <HomeView teams={teams} news={news} matches={matches} heroStatistics={heroStatistics} topPlayers={topPlayers} />
+  );
 };
 
 export default page;
