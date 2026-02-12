715c273dd20e675583aefaf599bb98887028b414
Tonkata-hub <tonyyankOV07@gmail.com>
2026-02-12 21:37:57 +0200

    wrapped a suspense

--- src/app/(gamemode-layout)/games/fantasy/my-team/page.tsx
+++ src/app/(gamemode-layout)/games/fantasy/my-team/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react';
 import FantasyView from '@/domains/fantasy/gamemode/FantasyView';
 
 const MyTeamPage = () => {
-  return <FantasyView />;
+  return (
+    <Suspense fallback={<div className="min-h-screen bg-[#07000f]" />}>
+      <FantasyView />
+    </Suspense>
+  );
 };
 
 export default MyTeamPage;
